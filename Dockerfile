90a27e1443965aa6494f52b54c099593206db727
HusanjonAzamov <azamovhusanboy08@gmail.com>
2024-11-14 16:32:06 +0500

    'change'

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-FROM python:3.11-slim
+# Python asosidagi rasmni oling
+FROM python:3.11
 
-
+# Kerakli kutubxonalar va asboblarni o'rnatish
 WORKDIR /code
-
-RUN apt update && apt upgrade -y
-
-COPY . .
-
-
+COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 
-CMD sh ./entrypoint.sh
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh  # Skriptni bajarish huquqini berish
+
+ENTRYPOINT ["/entrypoint.sh"]
--- handlers/notice/funk.py
+++ handlers/notice/funk.py
@@ -4,47 +4,61 @@ from services.services import getNotice
 from asgiref.sync import sync_to_async
 from main.models import Notice
 
+# TelegramClient ni yaratish
 client = TelegramClient(env.PHONE_NUMBER, env.API_ID, env.API_HASH)
 
+# Guruhlarning ID'lari
 chat_ids = [
-    -1002264446732, -1002037632361,
+    -1002202717276, -1002156534133, -1001368447491, -1002318451740, -1002474360096,
+    -1002285384904, -1002375234737, -1001696066827, -1001661240991, -1001950144985,
+    -1001889067613, -1002225373901, -1001698342886, -1001561994254, -1001661001936,
+    -1002036095755, -1002297602082, -1002345459660, -1001995597670, -1002220623140,
+    -1002178966866, -1002159005012, -1002206635995, -1001698975338, -1002067577089,
+    -1002232778089, -1002247652778, -1002169615850, -1002161701360, -1001927709760,
+    -1002083400408, -1002220553035, -1002089458891, -1002068631562, -1002232013574,
+    -1002230508278, -1002406606261, -1001934813270, -1002387686287, -1002215121340
 ]
+
+
+# chat_ids = [
+#     -1002264446732, -1002037632361, -4513123338
+# ]
 
 message_counters = {chat_id: 0 for chat_id in chat_ids}
 last_sents = {chat_id: False for chat_id in chat_ids}
-
 last_notice_data = {}
 
 
+# Xabar yuborish uchun funksiya
 async def send_notice(notice, chat_id):
-    global last_sent
     try:
-        await client.send_message(chat_id, notice.descriptions, parse_mode='markdown')
-        last_sents[chat_id] = True 
+        # Xabarni yuborish
+        await client.send_message(chat_id, notice.descriptions, parse_mode='html')
+        last_sents[chat_id] = True  
     except Exception as e:
         print(f"Error sending message to {chat_id}: {e}")
-        last_sents[chat_id] = False  
+        last_sents[chat_id] = False 
+
 
 @client.on(events.NewMessage())  
 async def handler(event):
     global message_counters, last_sents
-    if event.chat_id in chat_ids:  # Agar xabar kutilgan guruhdan kelsa
-        if not event.out:  # Agar xabar tashqaridan kelgan bo'lsa
+    if event.chat_id in chat_ids: 
+        if not event.out: 
             notices = await sync_to_async(getNotice)()  
             if notices:
-                message_counters[event.chat_id] += 1  # Guruhdagi xabar sonini oshir
+                message_counters[event.chat_id] += 1  
                 print(f"New message received in chat {event.chat_id}. Current count: {message_counters[event.chat_id]}")
 
-                notice_list = await sync_to_async(list)(Notice.objects.all())
+                notice_list = await sync_to_async(list)(Notice.objects.all())  
                 
                 if notice_list:  
-                    notice = notice_list[0]  # Dastlabki notice ni olish
+                    notice = notice_list[0]  
 
-                    # Har bir guruh uchun xabar yuborish shartini tekshirish
                     if message_counters[event.chat_id] >= notice.interval and not last_sents[event.chat_id]:
-                        await send_notice(notice, event.chat_id)
-                        message_counters[event.chat_id] = 0  # Xabar yuborilgandan keyin hisobni tozalash
+                        await send_notice(notice, event.chat_id) 
+                        message_counters[event.chat_id] = 0  
 
                     elif message_counters[event.chat_id] < notice.interval:
-                        last_sents[event.chat_id] = False  # Shart bajarilmasa, xabar yuborilmasin
+                        last_sents[event.chat_id] = False  
 
